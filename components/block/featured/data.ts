e7b58be344f81097388f9e50e9a7b03177e22404
Fachri Riyanto <fachri@redpumpkin.ai>
2026-02-08 06:32:03 +0700

    update: featured work data

--- components/block/featured/data.ts
+++ components/block/featured/data.ts
@@ -8,7 +8,7 @@ export const data: FeaturedPost[] = [
         "description": "Mataparser is my first AI product project, currently in an experimental phase. It is a service that extracts content from documents and images into structured JSON, providing an API that's easy to integrate into existing platforms.",
         "url": "https://mataparser.cloud",
         "featured": "/images/portfolio/mataparser.png",
-        "tech": ["NextJS", "Shadcn", "Better-Auth", "FastAPI", "LiteLLM", "PostgreSQL", "Docker"],
+        "tech": ["NextJS", "Shadcn", "Better-Auth", "FastAPI", "LiteLLM", "MCP", "PostgreSQL", "Docker"],
         "text_demo": "Website:"
     },
 ];
